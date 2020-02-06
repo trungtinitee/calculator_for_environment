b30bbbeeb87e44ff167c49f9f7196ec26872e81e
Lu Trung Tin <49675459+trungtinitee@users.noreply.github.com>
2020-02-06 12:17:34 +0700

    new toash login and fix items

---
+++ js/amination.js
@@ -0,0 +1,27 @@
+var checked = false;
+
+function ChangeOverFlowBody() {
+    if (checked === false) {
+        document.getElementsByTagName("body")[0].style.overflow = "hidden";
+        checked = true;
+    } else {
+        document.getElementsByTagName("body")[0].style.overflow = "auto";
+        checked = false;
+    }
+}
+
+function AppearBackgroundAndForm() {
+    document.getElementById("background_form_appear_id").style.opacity = 0.7;
+    document.getElementById("background_form_appear_id").style.visibility = "visible";
+    document.getElementById("form_appear_id").style.opacity = 1;
+    document.getElementById("form_appear_id").style.visibility = "visible";
+    document.getElementsByTagName("body")[0].style.overflow = "hidden";
+}
+
+function HiddenForm() {
+    document.getElementById("background_form_appear_id").style.opacity = 0;
+    document.getElementById("background_form_appear_id").style.visibility = "hidden";
+    document.getElementById("form_appear_id").style.opacity = 0;
+    document.getElementById("form_appear_id").style.visibility = "hidden";
+    document.getElementsByTagName("body")[0].style.overflow = "auto";
+}
