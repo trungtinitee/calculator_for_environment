ea60cb454aaf2054bd9f308d72962c9217dc78c4
Lu Trung Tin <49675459+trungtinitee@users.noreply.github.com>
2020-02-07 18:43:05 +0700

    new form calculator

---
+++ js/real_time_calculator.js
@@ -0,0 +1,72 @@
+/***************************** FUNCTION MAIN ************************/
+function AddTextLable(textInput, idTextField) {
+    var createNewPTag = document.createElement("h3");
+    createNewPTag.innerHTML = textInput;
+    createNewPTag.setAttribute("id", idTextField);
+    createNewPTag.setAttribute("class", "format_text");
+
+    var createNewDivTag = document.createElement("div");
+    createNewDivTag.setAttribute("class", "background_items");
+
+    //append child just create
+    createNewDivTag.appendChild(createNewPTag);
+    document.getElementById("change_content_id").appendChild(createNewDivTag);
+} //add text for form
+
+function AddInputField(idInputField, placeHolder) {
+    var createInputTag = document.createElement("input");
+    createInputTag.setAttribute("id", idInputField);
+    createInputTag.setAttribute("placeholder", placeHolder);
+    createInputTag.setAttribute("class", "format_input");
+
+
+
+    var createNewDivTag = document.createElement("div");
+    createNewDivTag.setAttribute("class", "background_items");
+
+    //append child just create
+    createNewDivTag.appendChild(createInputTag);
+    document.getElementById("change_content_id").appendChild(createNewDivTag);
+} //add new input field
+
+function ShowResultInForm(textInput, idTextField) {
+    var createNewPTag = document.createElement("h3");
+    createNewPTag.innerHTML = textInput;
+    createNewPTag.setAttribute("id", idTextField);
+    createNewPTag.setAttribute("class", "format_text");
+
+    var createNewDivTag = document.createElement("div");
+    createNewDivTag.setAttribute("class", "background_items");
+
+    //append child just create
+    createNewDivTag.appendChild(createNewPTag);
+    document.getElementById("result_content_id").appendChild(createNewDivTag);
+
+} // show results calculation in form
+/******************************DEFAULT FORM LOAD******************** */
+/******************************************************************** */
+
+// Tính toán song chắn rác
+AddTextLable("Nhập chiều dài", "");
+AddInputField("chieu_dai_id", "");
+AddTextLable("Nhập chiều rộng", "");
+AddInputField("chieu_rong_id", "");
+
+ShowResultInForm("Diện tích là: ", "dien_tich_id");
+
+/******************************  Catch event when user edit in form****** */
+/************************************************************************* */
+
+document.getElementById("chieu_dai_id").addEventListener("keyup", function DienTich() {
+    var chieuDai = document.getElementById("chieu_dai_id").value;
+    var chieuRong = document.getElementById("chieu_rong_id").value;
+    document.getElementById("dien_tich_id").innerHTML = "Diện tích là: " + chieuRong * chieuDai;
+
+})
+
+document.getElementById("chieu_rong_id").addEventListener("keyup", function DienTich() {
+    var chieuDai = document.getElementById("chieu_dai_id").value;
+    var chieuRong = document.getElementById("chieu_rong_id").value;
+    document.getElementById("dien_tich_id").innerHTML = "Diện tích là: " + chieuRong * chieuDai;
+
+})
